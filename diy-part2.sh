dd4aebfe22955b1bef47e329f8a584c35dbe71f4
yuer888999 <30942356+yuer888999@users.noreply.github.com>
2022-09-03 00:16:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.8/g' package/base-files/files/bin/config_generate
 
-# sed -i 's/OpenWrt /jade compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' $ZZZ
+sed -i 's/OpenWrt /jade compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' $ZZZ
